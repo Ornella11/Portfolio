8eb7cee7986bc63fa69c28ea6fa2f4da3fc69bb1
Ornella11 <124208143+Ornella11@users.noreply.github.com>
2024-01-13 23:39:31 +0100

    ajout pages du portfolio

--- boutton.js
+++ boutton.js
@@ -7,3 +7,23 @@ for (var i = 0; i < buttons.length; i++) {
         window.location.href = targetPage;
     });
 }
+
+
+// ------------------------ LIEN
+/*var buttons = document.querySelectorAll(".boutton-lien");
+
+for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function() {
+        var targetPage = this.getAttribute("data-target");
+        window.location.href = targetPage;
+    });
+}*/
+
+var buttons = document.querySelectorAll(".boutton-lien");
+
+for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function() {
+        var targetPage = this.getAttribute("data-target");
+        window.open(targetPage, "_blank");
+    });
+}
